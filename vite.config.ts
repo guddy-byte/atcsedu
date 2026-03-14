da22cd89a3486b0fc009de0b69ee458fc487e779
Goodness <idowugudness@gmial.com>
2026-03-14 18:05:03 +0100

    New Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import tailwindcss from '@tailwindcss/vite'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const base = process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/'
+
 export default defineConfig({
+  base,
   plugins: [vue(), vueJsx(), tailwindcss()],
 })
